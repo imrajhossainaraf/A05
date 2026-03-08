5498143aba631a5933dd32394d5c7d4ace4ca23a
Imraj Hossain Araf <imrajhossainaraf12@gmail.com>
2026-03-08 08:17:15 +0600

    Fix

--- script/login.js
+++ script/login.js
@@ -1,12 +1,13 @@
 let login = document.getElementById("login-btn")
-let id=document.getElementById("name")
-let pass=document.getElementById("pass")
+let id = document.getElementById("name")
+let pass = document.getElementById("pass")
 
-login.addEventListener("click", function () {
-    if(id.value=="admin" && pass.value=="admin123"){
-        window.location.href="dashboard.html"
-    }
-    else{
+login.addEventListener("click", function (e) {
+    e.preventDefault(); // stops form reload
+
+    if (id.value == "admin" && pass.value == "admin123") {
+        window.location.href = "./dashboard.html"
+    } else {
         alert("Invalid username or password")
     }
 })
